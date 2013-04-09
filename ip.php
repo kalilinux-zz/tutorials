92cd9cb8ebe4d351b4e4088048a8e0aeeac43193
kalilinux <kaligr3y@gmail.com>
2013-04-09 21:57:17 +0530

    Update ip.php

--- ip.php
+++ ip.php
@@ -5,24 +5,3 @@ $stringData ="Date:".date('Y-m-d H:i:s')."<br/>Ip:".$_SERVER['REMOTE_ADDR']."<br
 fwrite($fh, $stringData);
 fclose($fh);
 ?>
-<?php
-header( 'Location: http://crackthesec.blogspot.com/' ) ;
-$ip = $_SERVER['REMOTE_ADDR'];
-$hostaddress = gethostbyaddr($ip);
-$browser = $_SERVER['HTTP_USER_AGENT'];
-$referred = $_SERVER['HTTP_REFERER']; // a quirky spelling mistake that stuck in php
-
-print "<strong>Display IP address:</strong><br />\n";
-print "$ip<br /><br />\n";
-print "<strong>More detailed host address:</strong><br />\n";
-print "$hostaddress<br /><br />\n";
-print "<strong>Display browser info</strong>:<br />\n";
-print "$browser<br /><br />\n";
-print "<strong>Where you came from (if you clicked on a link to get here</strong>:<br />\n";
-if ($referred == "") {
-print "Page was directly requested";
-}
-else {
-print "$referred";
-}
-?> 
